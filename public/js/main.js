3e2d598b587a158edf1552ec00651e6ef42c2719
VicLo <victoria2666@gmail.com>
2020-02-09 17:54:50 -0500

    Fix small bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,22 @@
 var firebase = app_fireBase;
+let name = "";
+const msgScreen = document.getElementById("messages");
+const msgForm = document.getElementById("messageForm");
+const msgInput = document.getElementById("msg-input");
+const msgBtn = document.getElementById("msg-btn");
+const userName = document.getElementById("user-name");
+const db = firebase.database();
+const msgRef = db.ref("/msgs"); //save in msgs folder in database
+const id = uuid();
+
 function init(){
-    let name = "";
-    let uid = null;
+
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          // User is signed in. Get their name and userID.
-          uid = user.uid;
+          // User is signed in. Get their name.
           name = user.displayName;
-          console.log(name);
+          msgRef.on('child_added', updateMsgs);
+          userName.innerHTML = "Wleocme, " + name;
         }else{
             //redirect to login page
             uid = null;
@@ -16,6 +25,7 @@ function init(){
       });
 
     document.getElementById('log-out').addEventListener('click', logOut);
+    msgForm.addEventListener('submit', sendMessage);
 }
 
 function logOut(){
@@ -28,4 +38,29 @@ function logOut(){
       });
 }
 
+const updateMsgs = data =>{
+  const {id , name: myName, text} = data.val();
+  const msg = `<li class="${name == myName ? "msg my": "msg"}"><span class = "msg-span">
+    <i class = "name">${myName}: </i>${text}
+    </span>
+  </li>`
+  msgScreen.innerHTML += msg;
+}
+
 document.addEventListener('DOMContentLoaded',init);
+
+function sendMessage(e){
+  e.preventDefault();
+  const text = msgInput.value;
+
+    if(!text.trim()) return alert('Palese tpye yuor mesagse.'); //no msg submitted
+    const msg = {
+        id,
+        name,
+        text: text
+    };
+
+    msgRef.push(msg);
+    msgInput.value = "";
+}
+
